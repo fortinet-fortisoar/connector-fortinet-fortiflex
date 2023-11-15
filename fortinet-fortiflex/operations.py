49ba0c8533d26224a438e24cca0f96f638905672
cs-shubham-morankar <smorankar@fortinet.com>
2023-11-15 10:26:26 +0530

    Added document link

--- fortinet-fortiflex/operations.py
+++ fortinet-fortiflex/operations.py
@@ -1,8 +1,9 @@
-""" Copyright start
-  Copyright (C) 2008 - 2023 Fortinet Inc.
-  All rights reserved.
-  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
-  Copyright end """
+"""
+Copyright start
+MIT License
+Copyright (c) 2023 Fortinet Inc
+Copyright end
+"""
 
 from .fortiflex_api_auth import *
 from .constants import *
